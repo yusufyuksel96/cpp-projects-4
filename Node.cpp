140cb78d6c09dfb25c536fe38e8d1430a755ca82
nozlemozcan <nozlemozcan@users.noreply.github.com>
2018-10-15 13:43:43 +0300

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -33,7 +33,7 @@ Node::Node(Node&& node){
 
     node.name = "";
     node.amount = 0;
-    delete node->next;
+    delete node.next;
     node.next = NULL;
 
 }
@@ -48,7 +48,7 @@ Node& Node::operator=(Node&& node){
 
     node.name = "";
     node.amount = 0;
-    delete node->next;
+    delete node.next;
     node.next = NULL;
     return *this;
 }
